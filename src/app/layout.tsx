7a7bc8060860488c5d1f4bacd210309834b75569
toreharose-bravo <toreha.ro.se.1149@gmail.com>
2025-02-13 21:39:31 +0900

    adsf

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        asdfasdf
         {children}
       </body>
     </html>
